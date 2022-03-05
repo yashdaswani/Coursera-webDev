f4d52be666ac257b0a9791d52f7f2ce1b6af1faa
yashdaswani <100758924+yashdaswani@users.noreply.github.com>
2022-03-05 15:49:14 +0530

    updated

--- mod4/script.js
+++ mod4/script.js
@@ -3,10 +3,10 @@ names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
 names[3]="jason";
-names[4]="paul";
+names[4]="yash";
 names[5]="frank";
-names[6]="larry";
-names[7]="paula";
+names[6]="vicky";
+names[7]="jeetesh";
 names[8]="laura";
 names[9]="jim";
 
